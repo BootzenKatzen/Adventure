1a9663bd97593dcd01661ebbefe728aa31584e2b
BootzenKatzen <48966482+BootzenKatzen@users.noreply.github.com>
2022-08-03 14:22:43 -0400

    Latest Version
    Added more spacing, found a bug when run on windows powershell and fixed it. Added more content

--- Adventures2.py
+++ Adventures2.py
@@ -1,10 +1,13 @@
 def start_game():
     print("This is the ADVENTURE ZONE!")
-    print("")
-    Adventurer = input("What is your name?")
-    print("")
+    Space()
+    Adventurer = input("What is your name?\n")
+    Space()
     print("Hello", Adventurer,"!")
+    Space()
 
+def Space():
+     print("")
 
 def again():
     again = input("Would you like to play again?")
@@ -15,8 +18,8 @@ def again():
 
 def SayAgain():
     print("...")
-    print("What? I don't understand")
-    print("")
+    print("What? I don't understand.")
+    print("Try again")
 
 
 def the_beginning():
@@ -30,12 +33,30 @@ def the_beginning():
             elif quest.lower().strip() =="y":
                 print("")
                 print("Welcome Adventurer!")
+                Space()
+                break
             else:
                 SayAgain()
                 continue
-            return
-def Mead():
+def Sommelier():
     print("Placeholder")
+
+def DrinkingPal():
+    print("Placeholder")
+
+def Mead():
+    print("The bartender hands you a large mug of mead.")
+    Space()
+    print("The viking in the corner lifts his mug in salute.")
+    Space()
+    chug = input("Do you:\n \nA) Chug it \n\nOR \n\nB) Sip it")
+    if chug.lower().strip() == "a":
+        DrinkingPal()
+    elif chug.lower().strip() == "b":
+        Sommelier()
+    else:
+        SayAgain()
+        Mead()
 
 def Ale():
     print("Placeholder")
@@ -45,15 +66,19 @@ def Milk():
 
 def Drinks():
     print("The bartender offers you three choices:")
+    Space()
     print("A) Mead")
     print("B) Ale")
     print("C) Milk")
     drank = input("Which do you choose? [A/B/C]")
     if drank.lower().strip() == "a":
+        Space()
         Mead()
     elif drank.lower().strip() == "b":
+        Space()
         Ale()
     elif drank.lower().strip() == "c":
+        Space()
         Milk()
     else:
         SayAgain()
@@ -65,14 +90,18 @@ def Sober():
 def Bar():
     drink = input("Would you like to get a drink? [Y/N]")
     if drink.lower().strip() == 'y':
+        Space()
         Drinks()
     elif drink.lower().strip() == "n":
+        Space()
         Sober()
     else:
         SayAgain()
         Bar()
 
 def Tavern():
+    print("You enter the dimly lit tavern")
+    print("You see a lot of drunks")
     brawl = input("Would you like to start a fight? [Y/N]")
     if brawl.lower().strip() == "y":
         print("That was a bad idea.")
@@ -85,7 +114,9 @@ def Tavern():
         Bar()
     else:
         SayAgain()
+        Space()
         Tavern()
+
 def Woods():
     print("Placeholder")
 
@@ -93,28 +124,28 @@ def Meadow():
     print("Placeholder")
 
 def Outdoors():
+    print("You decide to explore outside instead")
+    print("You see:")
+    print("A) A dark and spooky woods")
+    print("B) A bright and sunny meadow")
     explore = input("Where would you like to go?")
     if explore.lower().strip() == "a":
         Woods()
     elif explore.lower().strip() == "b":
         Meadow()
 
-
 def choice1():
     tavern = input("Would you like to go into the tavern? [Y/N]")
     if tavern.lower().strip() == "y":
-        print("You enter the dimly lit tavern")
-        print("You see a lot of drunks")
+        Space()
         Tavern()
     elif tavern.lower().strip() == "n":
-        print("You decide to explore outside instead")
-        print("You see:")
-        print("A) A dark and spooky woods")
-        print("B) A bright and sunny meadow")
+        Space()
         Outdoors()
 
 def game():
     the_beginning()
+    Space()
     choice1()
 
 def start1():
