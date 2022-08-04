6b699719a020373d35690783bcf301d2fcab67b4
BootzenKatzen <48966482+BootzenKatzen@users.noreply.github.com>
2022-08-04 16:53:36 -0400

    Update 4
    Added pauses between lines, so the text comes up more slowly, instead of a giant block.  Combined the pause (PZ) feature with the space (SPC) feature to create RM() which leaves "room" for the text to breathe - a visual break between the text with a pause. I've added it in most places where I had the SPC() function.  Also added more content.  It's now caught up to the previous attempt at making a choose your own adventure.

--- Adventures2.py
+++ Adventures2.py
@@ -1,5 +1,10 @@
 import random #needed for random integers for LuckCheck
 
+from time import sleep
+
+def PZ():
+    sleep(2)
+
 def LuckCheck(): #flipping a coin for luck
     global coin
     flip = random.randint(0,1) #pulls either a 0 or 1 at random for coin flip
@@ -11,9 +16,14 @@ def LuckCheck(): #flipping a coin for luck
 def SPC(): #adding a quicker way to add line spaces
      print("")
 
+def RM():
+    PZ()
+    SPC()
+
 def Leave(): #adding a way out of the game
     leave = input("Would you like to leave? [Y/N]").lower().strip()
     if leave == "y":
+        RM()
         print("Goodbye!")
         quit()
     elif leave == "n":
@@ -37,11 +47,11 @@ def Quest():
         if quest == "n":
             SPC()
             print("Then why are you here?")
-            SPC()
+            RM()
             print("This is the adventure zone.")
-            SPC()
+            RM()
             print("The zone for adventures.")
-            SPC()
+            RM()
             print("You silly goose.")
             SPC()
             Leave()
@@ -49,7 +59,7 @@ def Quest():
             SPC()
             print("Well then, let's go", Adventurer,"!")
             print("I hope you have a wonderful time on your journey!")
-            SPC()
+            RM()
             SPC()
             break
         else:
@@ -59,8 +69,10 @@ def Quest():
 def Again(): #quick way to restart after an ending
     again = input("Would you like to adventure again? [Y/N]").lower().strip()
     if again == "y":
+        RM()
         game()
     elif again == "n":
+        RM()
         print("Okay, bye!")
         quit()
     else:
@@ -73,59 +85,166 @@ def Hold(): #adding a quick way to add a placeholder for playtesting
 
 def SayAgain(): #keeps loops from breaking if player makes a typo
     print("...")
+    PZ()
     print("What? I don't understand.")
+    PZ()
     print("Try again")
 
 def Sommelier():
     Hold()
 
 def Crush():
-    Hold()
+    print("Well, that wasn't the best idea.")
+    RM()
+    print("He responds in kind, and crushes the bones in your hand.")
+    print("You pass out from the pain")
+    RM()
+    print("...")
+    PZ()
+    print("...")
+    PZ()
+    print("...")
+    RM()
+    print("You wake up to see a very attractive person standing over you")
+    PZ()
+    print("They nurse you back to health, and you end up falling in love")
+    PZ()
+    print("You get married and have 9 kids together")
+    RM()
+    print("You didn't really get to go on an adventure, but you live happily ever after anyway!")
+    RM()
+    print("THE END")
 
 def Weakling():
+    print("The viking seems dissappointed in your handshake")
+    PZ()
+    print("... but he notices that you have very soft hands")
+    PZ()
+    print("He shrugs and sits next to you to talk")
+    PZ()
+    print("He buys you a round")
+    RM()
+    print("... and another")
+    RM()
+    print("... and another")
+    RM()
+    print("After several more rounds, some dancing, some lauging, and some drunken goat-tipping, you black out")
+    RM()
+    print("...")
+    PZ()
+    print("...")
+    PZ()
+    print("...")
+    RM()
+    print("You wake up with a raging headache, in an unfamiliar bed")
+    PZ()
+    print("You smell bacon and eggs, and hear someone whistling in the kitchen")
+    PZ()
+    print("You go to rub your eyes and notice a ring on your left hand")
+    PZ()
+    print("You have married the viking after a night of drunken revelry")
+    RM()
+    print("You live happily ever after (?)")
+    RM()
+    print("THE END")
+    RM()
+    print("(For now)")
+    RM()
+    Again()
+
+def Listen():
+    Hold()
+
+def Rude():
+    Hold()
+
+def Party():
+    print("Sven grabs another chair for you")
+    PZ()
+    print("He says'Hey,", Adventurer, "these are my pals, Jorgen, Ivan, and Dave ")
+    PZ()
+    print("The three of them nod at you")
+    PZ()
+    print("Sven asks'So what brings you here?'")
+    PZ()
+    print("You tell them you were looking for a quest")
+    RM()
+    print("The 4 of them look at each other for a moment")
+    RM()
+    print("'We may have a job for you' says Jorgen")
+    PZ()
+    print("'If you want it, that is' says Ivan")
+    PZ()
+    print("Dave stays silent, but he nods")
+    RM()
+    print("'Would you like to hear about it?' asks Sven")
+    answer = input("Y/N").lower().strip()
+    SPC()
+    if answer == "y":
+        RM()
+        Listen()
+    elif answer == "n":
+        RM()
+        Rude()
+
+def Shy():
     Hold()
 
 def GoodShake():
-    Hold()
+    print("The viking is surprised, but delighted by your warrior etiquette")
+    PZ()
+    print("He introduces himself as Sven Svenson")
+    PZ()
+    print("He buys you another round, and invites you to his table")
+    RM()
+    sit = input("Do you sit with him? [Y/N]").lower().strip()
+    if sit ==  "y":
+        RM()
+        Party()
+    elif sit == "n":
+        RM()
+        Shy()
+
 
 def BroShake():
     Hold()
 
 def DrinkingPal():
     print("The viking in the corner also chugs his mead, and slams the mug on the table")
+    PZ()
     print("He approaches you")
+    PZ()
     print("He is larger than you thought, and you thought he was very large")
-    SPC()
+    RM()
     print("He holds out his hand to you.  Do you:")
-    SPC()
+    RM()
     print("A)Grab his hand as hard as you can")
     print("B)Give him the limp fish handshake")
     print("C)Grab his forearm")
     print("D)Give a firm handshake and try for the bro hug")
     shake = input().lower().strip()
     if shake == "a":
-        SPC()
+        RM()
         Crush()
     elif shake == "b":
-        SPC()
+        RM()
         Weakling()
     elif shake == "c":
-        SPC()
+        RM()
         GoodShake()
     elif shake == "d":
-        SPC()
+        RM()
         BroShake()
     else:
-        SPC()
-        SayAgain()
+        RM()
         DrinkingPal()
 
 
 def Mead():
     print("The bartender hands you a large mug of mead.")
-    SPC()
+    RM()
     print("The viking in the corner lifts his mug in salute.")
-    SPC()
+    RM()
     print("Do you:")
     print("A) Chug it")
     print("OR")
@@ -133,8 +252,10 @@ def Mead():
     SPC()
     chug = input().lower().strip()
     if chug == "a":
+        RM()
         DrinkingPal()
     elif chug == "b":
+        RM()
         Sommelier()
     else:
         SayAgain()
@@ -158,10 +279,13 @@ def Ale():
     SPC()
     takeaseat = input().lower().strip()
     if takeaseat == "a":
+        RM()
         Stool()
     elif takeaseat == "b":
+        RM()
         Table()
     elif takeaseat == "c":
+        RM()
         Awkward()
     else:
         SayAgain()
@@ -172,20 +296,20 @@ def Milk():
 
 def Drinks():
     print("The bartender offers you three choices:")
-    SPC()
+    RM()
     print("Would you like:")
     print("A) Mead")
     print("B) Ale")
     print("C) Milk")
     drank = input().lower().strip()
     if drank == "a":
-        SPC()
+        RM()
         Mead()
     elif drank == "b":
-        SPC()
+        RM()
         Ale()
     elif drank == "c":
-        SPC()
+        RM()
         Milk()
     else:
         SayAgain()
@@ -197,10 +321,10 @@ def Sober():
 def Bar():
     drink = input("Would you like to get a drink? [Y/N]").lower().strip()
     if drink == 'y':
-        SPC()
+        RM()
         Drinks()
     elif drink == "n":
-        SPC()
+        RM()
         Sober()
     else:
         SayAgain()
@@ -212,23 +336,24 @@ def Tavern():
     SPC()
     brawl = input("Would you like to start a fight? [Y/N]").lower().strip()
     if brawl == "y":
-        print("BAD END 1:")
-        SPC()
+        RM()
         print("That was a bad idea.")
         print("There was a very large viking in the corner that you failed to notice.")
         print("He bludgeons you in the head trying to stop the fight.")
-        SPC()
+        RM()
         print("But he is too strong for his own good.")
-        SPC()
+        RM()
         print("You died.")
         SPC()
         print("THE END")
-        SPC()
+        RM()
+        Print("(For now)")
+        RM()
         Again()
     elif brawl == "n":
-        SPC()
+        RM()
         print("Good choice, a very large, slightly grumpy viking was sitting in the corner.")
-        SPC()
+        RM()
         Bar()
     else:
         SayAgain()
@@ -241,31 +366,34 @@ def WakeUpCall():
 def GoodLuck():
     print("Nice call!")
     print("You've got some good luck there!")
+    RM()
     WakeUpCall()
 
 def Badluck(): #This will only show up the first time you fail the coin filp
     print("Oh dear.")
     print("What rotten luck.")
-    SPC()
-    SPC()
-    print("BAD END 2:")
-    SPC()
+    RM()
     print("You lie there asleep deep in the meadow")
     print("Sometimes you hear voices")
     print("but no one comes close enough to find you.")
     print("You slowly waste away, lost in dreams")
-    SPC()
+    RM()
     print("You died.")
-    SPC()
+    RM()
+    print("THE END")
+    RM("(For now)")
+    RM()
     print("...")
     SPC()
-    print("Would you like to try the coin flip again? (You'll keep the same call)")
+    print("Would you like to try your luck with coin flip again?")
+    print()"(You'll keep the same call)")
     SPC()
     AnotherFlip()
 
 def AnotherFlip(): #so they can try their luck again without going through the whole adventure
     flipagain = input().lower().strip()
     if flipagain == "y":
+        RM()
         CoinFlip2()
     else:
         print("Maybe next time, huh?")
@@ -281,30 +409,31 @@ def CoinFlip2(): #so it doesn't give the whole "BAD END" a second time
         print("Bad luck again?")
         print("Drat.")
         print("Want another flip?")
+        RM()
         AnotherFlip()
 
 def CoinFlip1(): #initial coin flip leading to good or bad end
     LuckCheck()
     print("You called ", call, "and it came up ", coin)
     if coin == call:
-        SPC()
+        RM()
         GoodLuck()
     else:
-        SPC()
+        RM()
         Badluck()
 
 def Nap():
     print("Since it's a lovely day you decide to take a nap")
     print("You lay down on a comfy patch, and the breeze lulls you to sleep")
-    SPC()
+    RM()
     print("...")
-    SPC()
+    RM()
     print("Unfortunately, this is a feild of magical poppies")
     print("Once you fall asleep here you cannot wake up without external intervention")
-    SPC()
+    RM()
     print("Now it's all up to your luck")
     print("We'll flip a coin!")
-    SPC()
+    RM()
     global call
     call = input("Heads or tails?").lower().strip()
     SPC()
@@ -314,13 +443,56 @@ def Botany():
     Hold()
 
 def Woods():
-    Hold()
+    print("The woods are very dark and dangerous")
+    PZ()
+    print("You see an axe leaning against a tree by the start of the path")
+    PZ()
+    axe = input("Do you take it?")
+    RM()
+    print("You enter the forest")
+    RM()
+    print("It is spooky, so you creep along")
+    RM()
+    print("Suddenly you see a large gnarled bear in front of you!")
+    bearfight = input("Do you dare to fight it?")
+    if bearfight.lower().strip() == "y" or "yes":
+        if axe.lower().strip() == "y" or "yes":
+            RM()
+            print("With your axe at the ready, you pounce on the bear!")
+            PZ()
+            print("It's a harrowing battle, but somehow you barely manage to win")
+            PZ()
+            print("You take the bear's head as a tropy and head back to town, triumphant")
+            RM()
+            print("Congratulations on your first small but successful adventure!")
+            Hold()
+        elif axe.lower().strip() == "n" or "no":
+            print("Raring for adventure you leap into action!")
+            PZ()
+            print("Unfortunately it's very hard to fight a bear barehanded")
+            RM()
+            print("If only you were 'bearhanded' instead.")
+            RM()
+            print("You died!")
+            RM()
+            Print("THE END")
+            RM()
+            Print("(For now)")
+            RM()
+            Again()
+    elif bearfight.lower().strip() == "n" or "no":
+        print("You decide you'd rather not fight a bear on your own")
+        print("A very wise decision")
+        print("Perhaps you should go back to the tavern for reinforcements")
+        Hold()
 
 def Meadow():
     print("The meadow is brightly lit, and full of flowers.")
+    PZ()
     print("There is a pleasant breeze blowing through.")
+    PZ()
     print("There doesn't appear to be anything dangerous nearby.")
-    SPC()
+    RM()
     print("Would you like to:")
     print("A) Take a nap")
     print("B) Examine the flowers")
@@ -329,14 +501,16 @@ def Meadow():
     SPC()
     flowerchild = input().lower().strip()
     if flowerchild == "a":
-        SPC()
+        RM()
         Nap()
     elif flowerchild == "b":
-        SPC()
+        RM()
         Botany()
     elif flowerchild == "c":
+        RM()
         Woods()
     elif flowerchild == "d":
+        RM()
         Tavern()
     else:
         SayAgain()
@@ -344,13 +518,15 @@ def Meadow():
 
 def Outdoors():
     print("Either you think it's too early to start drinking")
+    PZ()
     print("Or you're trying to avoid the stranger in a tavern cliche")
+    PZ()
     print("So you decide to explore outside instead")
-    SPC()
+    RM()
     print("You see:")
     print("A) A dark and spooky woods")
     print("B) A bright and sunny meadow")
-    SPC()
+    RM()
     explore = input("Where would you like to go?").lower().strip()
     if explore == "a":
         Woods()
@@ -359,22 +535,24 @@ def Outdoors():
 
 def choice1():
     print("You are a young human, coming of age in a land of myth and magic")
+    PZ()
     print("You decide it's time to seek your fortune out in the world")
+    PZ()
     print("You leave your house, waving goodbye to your family.")
-    SPC()
+    RM()
     print("You start by following the path away from your home")
     print("It leads into a small quaint town")
-    SPC()
+    RM()
     print("Smack dab in the middle of it is a large tavern")
     SPC()
     print("Would you like to go into the tavern? [Y/N]")
     SPC()
     tavern = input().lower().strip()
     if tavern == "y":
-        SPC()
+        RM()
         Tavern()
     elif tavern == "n":
-        SPC()
+        RM()
         Outdoors()
 
 def game():
