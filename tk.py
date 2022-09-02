390f97b46538d7a6881e7501756d0ca1f4861835
BootzenKatzen <48966482+BootzenKatzen@users.noreply.github.com>
2022-09-02 17:12:15 -0400

    GUI update 1
    Finally got it working, and it's starting to look how I want it to, too.

--- tk.py
+++ tk.py
@@ -7,9 +7,12 @@ from time import sleep
 def PZ():
     sleep(1)
 
+font1 = ("Arial", 16, "bold")
+font2 = ("Arial", 12, "normal")
+font3 = ("Algerian", 20)
 # need to look into the .after method to add the pauses between lines.
 
-def print1(text):
+def print1(text): # open's scrollbox for editing, prints on new line, closes
     txt.configure(state = 'normal')
     txt.insert(tk.END,"\n")
     txt.insert(tk.END, text)
@@ -17,7 +20,7 @@ def print1(text):
     txt.see(tk.END)
     #PZ()
 
-def print2(text):
+def print2(text): # same as print 1 but leaves a blank line after
     txt.configure(state = 'normal')
     txt.insert(tk.END,"\n")
     txt.insert(tk.END, text)
@@ -25,23 +28,39 @@ def print2(text):
     txt.configure(state = 'disabled')
     txt.see(tk.END)
     #PZ()
+
+def changecmd(function):
+    button["command"] = function
+    root.bind('<Return>', function)
+
+def uip(): # stands for User InPut - shorthand for pulling input from Entry Widget
+    return userin.get()
+
+def uiplow(): #User InPut but lower case - strips input for downstream uses
+    return userin.get().lower().strip()
+
+def uiprint(): #gets user input, prints it, and then strips it
+    print1(uip())
+
 
 root = tk.Tk()
 
-frame = tk.Frame(root)
-frame.pack()
+#frame = tk.Frame(root)
+#frame.pack()
 bottomframe = tk.Frame(root)
-bottomframe.pack( side = tk.BOTTOM )
+bottomframe.grid(column=0, row =2, sticky = 'ew')
 root.title("Testing Zone")
-root.geometry('350x300')
-txt = st.ScrolledText(frame, width='40', height='15', wrap=tk.WORD)
-toplabel = tk.Label(frame, text = "Welcome!")
-userin = tk.Entry(bottomframe, width = '50')
-userin.pack(side = tk.LEFT)
+root.geometry('500x500')
+txt = st.ScrolledText(root, width='40', height='15', wrap=tk.WORD)
+txt.configure(font = font2)
+toplabel = tk.Label(root, text = "Welcome!")
+toplabel.configure(font = font3)
+userin = tk.Entry(bottomframe, width = '60')
+userin.grid(column=0, row=0, sticky="ew")
 
-txt.pack(side = tk.BOTTOM)
+txt.grid(column=0, row=1, sticky="nsew")
 txt.yview(tk.END)
-toplabel.pack(side = tk.TOP)
+toplabel.grid(column = 0, row = 0, ipady = 10, sticky='ew')
 
 txt.insert(tk.INSERT, 'Welcome to the Adventure Zone!\n')
 txt.insert(tk.INSERT, 'Would you like to play a game?')
@@ -50,7 +69,7 @@ txt.configure(state = 'disabled')
 def changelabel(text):
     toplabel["text"] = text
 
-def clear_text():
+def empty():
     userin.delete(0, tk.END)
 
 def woods(event=None):
@@ -62,63 +81,48 @@ def tavern(event=None):
     pass
 
 def choice1(event=None):
-    whereto = userin.get().lower().strip()
+    uiprint()
+    whereto = uiplow()
+    empty()
     if whereto == "woods":
         print1("The woods are spooky")
-        button["command"] = woods
+        changecmd(woods)
     elif whereto == "tavern":
         print1("The tavern is crowded")
-        button["command"] = tavern
+        changecmd(tavern)
     else:
         print1("What?")
         print1("Woods or Tavern?")
 
 
 def clicked(event=None):
-    start = userin.get().lower().strip()
-    print1(userin.get())
-    clear_text()
+    uiprint()
+    start = uiplow()
+    empty()
+    toplabel.configure(font = font1)
     changelabel("Let's go!")
     if start == "y":
         print1("Lets go!")
         print1("Woods or Tavern?")
-        button["command"] = choice1
+        changecmd(choice1)
     elif start == "n":
         print1("Ok bye!")
     else:
         print1("What?")
         print1("Do you want to play a game?")
 
-#root.bind('<Return>', clicked)
-button = tk.Button(bottomframe, text = "enter", command = clicked)
-button.pack(side = tk.RIGHT)
-#button2 = tk.Button(bottomframe, text = "enter", command = choice1)
-#button3 = tk.Button(bottomframe, text = "enter", command = woods)
-#button4 = tk.Button(bottomframe, text = "enter", command = tavern)
 
+button = tk.Button(bottomframe, text = "Enter", font = font2, command = clicked)
+button.grid(column = 1, row = 0, ipadx = 10, sticky = "e")
 
-"""
-def game():
-    print1("Do you want to play a game?")
-    start = userin.get().lower().strip()
-    if start == "y":
-        print1("Let's go!")
-        changelabel("Adventure Time!")
-    elif start == "n":
-        print1("Okay bye!")
-        changelabel("See ya!")
-    else:
-        print1("What?")
-        game()
+root.bind('<Return>', clicked)
 
-game()
-"""
+bottomframe.grid_columnconfigure(0, weight =1)
+bottomframe.grid_columnconfigure(1, weight = 0)
+
+root.grid_rowconfigure(0, weight = 0)
+root.grid_rowconfigure(1, weight = 2)
+root.grid_rowconfigure(2, weight = 0)
+root.grid_columnconfigure(0, weight = 1)
+
 root.mainloop()
-
-
-
-
-
-
-
-#txt.insert(INSERT, "More text")
