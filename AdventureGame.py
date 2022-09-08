6fc759647df064df8bc597df9e398f03ecfa3b0a
Hannah Wilpan <hannah.knowlton@jax.org>
2022-09-08 10:36:01 -0400

    Adding starting comments

---
+++ AdventureGame.py
@@ -0,0 +1,10 @@
+# Alrighty! Let's get this started!  I'm going to try and merge the GUI from
+# the tk.py file with the content from the Adventures2.py file.
+
+#I'll kind of have to think of the content backwards, since that's how it works
+#once the buttons are involved.
+
+#Instead of fluff text -> choice given -> choice -> new function -> new text
+
+#It has to be text -> choice given -> button press for choice ->
+#new text -> new choice -> new function assigned to button
