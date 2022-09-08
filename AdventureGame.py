f2d3dff48560acbde6dae567158d335310f23316
Hannah Wilpan <hannah.knowlton@jax.org>
2022-09-08 10:43:13 -0400

    Creating print methods
    In order to be able to add new text to the text box but keep it from
    being editable by the player, I created print1() and print() which
    enable the scrollbox, print what I want, and then disable it again.
    Print 2 does this, but then leaves a blank line in order to create some
    spacing in the text

--- AdventureGame.py
+++ AdventureGame.py
@@ -17,3 +17,21 @@ import tkinter.scrolledtext as st #for text box
 font1 = ("Arial", 16, "bold")  #normal font for label
 font2 = ("Arial", 12, "normal")#for button and text
 font3 = ("Algerian", 20) #starting font for label for big "Welcome"
+
+
+#creating methods to add text to the text box, but keep it from being
+#editable by the player
+def print1(text): # open's scrollbox for editing, prints on new line, closes
+    txt.configure(state = 'normal')
+    txt.insert(tk.END,"\n")
+    txt.insert(tk.END, text)
+    txt.configure(state = 'disabled')
+    txt.see(tk.END)
+
+def print2(text): # same as print 1 but leaves a blank line after
+    txt.configure(state = 'normal')
+    txt.insert(tk.END,"\n")
+    txt.insert(tk.END, text)
+    txt.insert(tk.END,"\n")
+    txt.configure(state = 'disabled')
+    txt.see(tk.END)
